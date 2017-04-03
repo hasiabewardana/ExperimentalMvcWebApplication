f8e2c5f71dfb63f186da26c3ead702c1c92663dc
hasiabewardana <hasi.prasanga@gmail.com>
2017-04-03 20:27:40 +0530

    create a mvc web application

---
+++ ExperimentalMvcWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExperimentalMvcWebApplication.Startup))]
+namespace ExperimentalMvcWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
